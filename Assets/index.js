d284830da467276057f1efcb89084bbccd7fdd29
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-23 23:11:08 +0300

    fetch data

--- Assets/index.js
+++ Assets/index.js
@@ -7,6 +7,6 @@ function getDofDay(){
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then(res =>res.json())
     .then(data => {
-        console.log(data)
+        //console.log(data)
     })
 } 
