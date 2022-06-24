1475ed3a1f696286ad743183fe0260e7a6d229cd
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-24 14:58:55 +0300

    add ingredients styling

--- Assets/index.js
+++ Assets/index.js
@@ -31,11 +31,46 @@ function appendElements(info){
        
        const description = document.getElementById('description')
        description.textContent = element.strInstructions
+
+       const ingredientContainer = document.getElementById('ingredients')
        
-       console.log(element.strIngredient1)
-       console.log(element.strIngredient2)
-       console.log(element.strIngredient3)
-       console.log(element.strIngredient4)
-       console.log(element.strIngredient5)
+       if(element.strIngredient1 !== null){
+           const ingOne = document.createElement('li')
+           ingOne.textContent = element.strIngredient1
+           ingredientContainer.appendChild(ingOne)
+       }
+       
+       if (element.strIngredient2 !== null){
+           const ingTwo = document.createElement('li')
+           ingTwo.textContent = element.strIngredient2
+           ingredientContainer.appendChild(ingTwo)
+       }
+       if (element.strIngredient3 !== null) {
+         const ingThree = document.createElement("li");
+         ingThree.textContent = element.strIngredient3;
+         ingredientContainer.appendChild(ingThree);
+       }
+       if (element.strIngredient4 !== null) {
+         const ingFour = document.createElement("li");
+         ingFour.textContent = element.strIngredient4;
+         ingredientContainer.appendChild(ingFour);
+       }
+       if (element.strIngredient5 !== null) {
+         const ingFive = document.createElement("li");
+         ingFive.textContent = element.strIngredient5;
+         ingredientContainer.appendChild(ingFive);
+       }
+       if (element.strIngredient6 !== null) {
+         const ingSix = document.createElement("li");
+         ingSix.textContent = element.strIngredient6;
+         ingredientContainer.appendChild(ingSix);
+       }
+       if (element.strIngredient7 !== null) {
+         const ingSeven = document.createElement("li");
+         ingSeven.textContent = element.strIngredient7;
+         ingredientContainer.appendChild(ingSeven);
+       }
+      
+
     })
 }
