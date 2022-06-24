cdf6f0be27e5ad233ccd908e74a8903e9b11b445
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-24 12:43:43 +0300

    change font color of drink details

--- Assets/index.js
+++ Assets/index.js
@@ -7,6 +7,25 @@ function getDofDay(){
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then(res =>res.json())
     .then(data => {
-        ///console.log(data)
+        appendElements(data.drinks)
+    
     })
 } 
+
+//Render Elements
+
+function appendElements(info){
+    info.forEach(element => {
+       const name = document.getElementById('name')
+       name.textContent = element.strDrink
+       console.log(element.strGlass)
+       console.log(element.strDrinkThumb)
+       console.log(element.strAlcoholic)
+       console.log(element.strInstructions)
+       console.log(element.strIngredient1)
+       console.log(element.strIngredient2)
+       console.log(element.strIngredient3)
+       console.log(element.strIngredient4)
+       console.log(element.strIngredient5)
+    })
+}
